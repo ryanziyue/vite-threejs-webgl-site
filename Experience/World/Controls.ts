3c7954ed5652bb6c9a6bf2cdb69b9aa1d62451ae
Ryan <rcheng888446@gmail.com>
2023-04-08 18:34:07 -0700

    added animations, about me, my work

--- Experience/World/Controls.ts
+++ Experience/World/Controls.ts
@@ -1,17 +1,42 @@
 import Experience from "../Experience";
+import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default class Controls {
     experience: any;
     scene: any;
     resources: any;
     camera: any;
+    room: any;
+    timeline: any;
+    sizes: any;
 
     constructor() {
         this.experience = new Experience("");
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
         this.camera = this.experience.camera;
+        this.room = this.experience.world.room.actualRoom;
+        this.sizes = this.experience.sizes;
 
+        GSAP.registerPlugin(ScrollTrigger);
+
+        this.setPath();
+
+    }
+
+    setPath() {
+        this.timeline = new (GSAP.timeline as any)();
+        this.timeline.to(this.room.position, {
+            x: this.sizes.width * 0.00094,
+            scrollTrigger: {
+                trigger: ".first-move",
+                markers: true,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 1,
+            }
+        });
     }
 
     resize() {
