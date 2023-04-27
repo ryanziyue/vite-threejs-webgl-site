ace9bd47b92931fc0b3998aa80a37f324f0ebe31
Ryan <rcheng888446@gmail.com>
2023-04-27 11:44:23 -0700

    re-added Experience (upper case)

---
+++ Experience/Experience.ts
@@ -0,0 +1,64 @@
+import { Scene } from "three";
+
+import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
+import assets from "./Utils/assets";
+import Resources from "./Utils/Resources";
+
+import Camera from "./Camera";
+import Renderer from "./Renderer";
+import Theme from "./Theme";
+
+import World from "./World/World";
+
+export default class Experience {
+	canvas: any;
+	scene: any;
+	sizes: any;
+	time: any;
+	camera: any;
+	renderer: any;
+	theme: any;
+	resources: any;
+	world: any;
+	static instance: any;
+
+	constructor(canvas: any) {
+		if (Experience.instance) {
+			return Experience.instance;
+		}
+
+		Experience.instance = this;
+		this.canvas = canvas;
+		this.scene = new Scene();
+		this.sizes = new Sizes();
+		this.time = new Time();
+		this.camera = new Camera();
+		this.renderer = new Renderer();
+		this.resources = new Resources(assets);
+		this.theme = new Theme();
+		this.world = new World();
+
+		this.time.on("update", () => {
+			this.update();
+		})
+
+		this.sizes.on("resize", () => {
+			this.resize();
+		})
+
+	}
+
+	resize() {
+		this.camera.resize();
+		this.renderer.resize();
+		this.world.resize();
+	}
+
+	update() {
+		this.camera.update();
+		this.renderer.update();
+		this.world.update();
+	}
+
+}
