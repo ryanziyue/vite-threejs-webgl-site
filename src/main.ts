56e7de05c8dbe5b61d8cbc7534dfa80dc926743b
Ryan <rcheng888446@gmail.com>
2023-05-07 17:45:41 -0700

    removed lenis scroll

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,4 @@
 import "./style.css";
 import Experience from "../Experience/Experience";
-import Lenis from "@studio-freight/lenis";
-
-window.onbeforeunload = function() {
-    window.scrollTo(0, 0);
-}
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
-experience;
-
-const lenis = new Lenis({
-    easing: (t) => (1 - Math.pow(1 - t, 5)),
-})
-
-function raf(time:any) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
